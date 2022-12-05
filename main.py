b3b77b518155bef100424e141934f4b9de0fc00e
ImLyenx <eric.hubert4205@gmail.com>
2022-12-05 14:26:52 +0100

    moved everything to the center of the window

--- main.py
+++ main.py
@@ -13,57 +13,57 @@ class Board:
 
     def placeCross(self, pos):
         self.color = (255, 0, 0)
-        if pos[0] < 196:
+        if 304 < pos[0] < 496:
             if pos[1] < 196:
                 if self.slots[0][0] == 0:
-                    pg.draw.line(window, self.color, (45, 45), (155, 155), 12)
-                    pg.draw.line(window, self.color, (45, 155), (155, 45), 12)
+                    pg.draw.line(window, self.color, (345, 45), (455, 155), 12)
+                    pg.draw.line(window, self.color, (345, 155), (455, 45), 12)
                     self.slots[0][0] = 1
             if 204 < pos[1] < 396:
                 if self.slots[1][0] == 0:
-                    pg.draw.line(window, self.color, (45, 245), (155, 355), 12)
-                    pg.draw.line(window, self.color, (45, 355), (155, 245), 12)
+                    pg.draw.line(window, self.color, (345, 245), (455, 355), 12)
+                    pg.draw.line(window, self.color, (345, 355), (455, 245), 12)
                     self.slots[1][0] = 1
             if 404 < pos[1]:
                 if self.slots[2][0] == 0:
-                    pg.draw.line(window, self.color, (45, 445), (155, 555), 12)
-                    pg.draw.line(window, self.color, (45, 555), (155, 445), 12)
+                    pg.draw.line(window, self.color, (345, 445), (455, 555), 12)
+                    pg.draw.line(window, self.color, (345, 555), (455, 445), 12)
                     self.slots[2][0] = 1
             else:
                 pass
-        elif 204 < pos[0] < 396:
+        elif 504 < pos[0] < 696:
             if pos[1] < 196:
                 if self.slots[0][1] == 0:
-                    pg.draw.line(window, self.color, (245, 45), (355, 155), 12)
-                    pg.draw.line(window, self.color, (245, 155), (355, 45), 12)
+                    pg.draw.line(window, self.color, (545, 45), (655, 155), 12)
+                    pg.draw.line(window, self.color, (545, 155), (655, 45), 12)
                     self.slots[0][1] = 1
             if 204 < pos[1] < 396:
                 if self.slots[1][1] == 0:
-                    pg.draw.line(window, self.color, (245, 245), (355, 355), 12)
-                    pg.draw.line(window, self.color, (245, 355), (355, 245), 12)
+                    pg.draw.line(window, self.color, (545, 245), (655, 355), 12)
+                    pg.draw.line(window, self.color, (545, 355), (655, 245), 12)
                     self.slots[1][1] = 1
             if 404 < pos[1]:
                 if self.slots[2][1] == 0:
-                    pg.draw.line(window, self.color, (245, 445), (355, 555), 12)
-                    pg.draw.line(window, self.color, (245, 555), (355, 445), 12)
+                    pg.draw.line(window, self.color, (545, 445), (655, 555), 12)
+                    pg.draw.line(window, self.color, (545, 555), (655, 445), 12)
                     self.slots[2][1] = 1
             else:
                 pass
-        elif 404 < pos[0]:
+        elif 704 < pos[0] < 896:
             if pos[1] < 196:
                 if self.slots[0][2] == 0:
-                    pg.draw.line(window, self.color, (445, 45), (555, 155), 12)
-                    pg.draw.line(window, self.color, (445, 155), (555, 45), 12)
+                    pg.draw.line(window, self.color, (745, 45), (855, 155), 12)
+                    pg.draw.line(window, self.color, (745, 155), (855, 45), 12)
                     self.slots[0][2] = 1
             if 204 < pos[1] < 396:
                 if self.slots[1][2] == 0:
-                    pg.draw.line(window, self.color, (445, 245), (555, 355), 12)
-                    pg.draw.line(window, self.color, (445, 355), (555, 245), 12)
+                    pg.draw.line(window, self.color, (745, 245), (855, 355), 12)
+                    pg.draw.line(window, self.color, (745, 355), (855, 245), 12)
                     self.slots[1][2] = 1
             if 404 < pos[1]:
                 if self.slots[2][2] == 0:
-                    pg.draw.line(window, self.color, (445, 445), (555, 555), 12)
-                    pg.draw.line(window, self.color, (445, 555), (555, 445), 12)
+                    pg.draw.line(window, self.color, (745, 445), (855, 555), 12)
+                    pg.draw.line(window, self.color, (745, 555), (855, 445), 12)
                     self.slots[2][2] = 1
             else:
                 pass
@@ -71,48 +71,48 @@ class Board:
             pass
 
     def placeCircle(self, pos):
-        if pos[0] < 196:
+        if 304 < pos[0] < 496:
             if pos[1] < 196:
                 if self.slots[0][0] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (100, 100), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (400, 100), 60, 8)
                     self.slots[0][0] = 2
             if 204 < pos[1] < 396:
                 if self.slots[1][0] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (100, 300), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (400, 300), 60, 8)
                     self.slots[1][0] = 2
             if 404 < pos[1]:
                 if self.slots[2][0] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (100, 500), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (400, 500), 60, 8)
                     self.slots[2][0] = 2
             else:
                 pass
-        elif 204 < pos[0] < 396:
+        elif 504 < pos[0] < 696:
             if pos[1] < 196:
                 if self.slots[0][1] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (300, 100), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (600, 100), 60, 8)
                     self.slots[0][1] = 2
             if 204 < pos[1] < 396:
                 if self.slots[1][1] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (300, 300), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (600, 300), 60, 8)
                     self.slots[1][1] = 2
             if 404 < pos[1]:
                 if self.slots[2][1] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (300, 500), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (600, 500), 60, 8)
                     self.slots[2][1] = 2
             else:
                 pass
-        elif 404 < pos[0]:
+        elif 704 < pos[0] < 896:
             if pos[1] < 196:
                 if self.slots[0][2] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (500, 100), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (800, 100), 60, 8)
                     self.slots[0][2] = 2
             if 204 < pos[1] < 396:
                 if self.slots[1][2] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (500, 300), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (800, 300), 60, 8)
                     self.slots[1][2] = 2
             if 404 < pos[1]:
                 if self.slots[2][2] == 0:
-                    pg.draw.circle(window, (0, 255, 0), (500, 500), 60, 8)
+                    pg.draw.circle(window, (0, 255, 0), (800, 500), 60, 8)
                     self.slots[2][2] = 2
             else:
                 pass
@@ -120,30 +120,34 @@ class Board:
             pass
 
     def drawGrid(self, color):
-        pg.draw.rect(window, color, pg.Rect(0, 196, 600, 8))
-        pg.draw.rect(window, color, pg.Rect(0, 396, 600, 8))
-        pg.draw.rect(window, color, pg.Rect(196, 0, 8, 600))
-        pg.draw.rect(window, color, pg.Rect(396, 0, 8, 600))
+        pg.draw.rect(window, color, pg.Rect(300, -4, 600, 8))
+        pg.draw.rect(window, color, pg.Rect(300, 196, 600, 8))
+        pg.draw.rect(window, color, pg.Rect(300, 396, 600, 8))
+        pg.draw.rect(window, color, pg.Rect(300, 596, 600, 8))
+        pg.draw.rect(window, color, pg.Rect(296, 0, 8, 600))
+        pg.draw.rect(window, color, pg.Rect(496, 0, 8, 600))
+        pg.draw.rect(window, color, pg.Rect(696, 0, 8, 600))
+        pg.draw.rect(window, color, pg.Rect(896, 0, 8, 600))
         pg.display.flip()
 
     def drawPlayerMark(self, player):
         pg.draw.rect(
-            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(196, 196, 8, 8)
+            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(496, 196, 8, 8)
         )
         pg.draw.rect(
-            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(396, 196, 8, 8)
+            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(696, 196, 8, 8)
         )
         pg.draw.rect(
-            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(196, 396, 8, 8)
+            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(496, 396, 8, 8)
         )
         pg.draw.rect(
-            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(396, 396, 8, 8)
+            window, (255, 0, 0) if player == 1 else (0, 255, 0), pg.Rect(696, 396, 8, 8)
         )
         pg.display.flip()
 
 
 board = Board()
-window = pg.display.set_mode((600, 600))
+window = pg.display.set_mode((1200, 600))
 board.drawGrid((255, 255, 255))
 run = True
 player = 1
