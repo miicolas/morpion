ad4b0c9f03dcfd73251c93f121cbd61e82166516
Lyenx <105422453+ItsLexus@users.noreply.github.com>
2022-12-08 21:37:31 +0100

    big refactor + added some win conditions

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame as pg
-import random
 
 
 class Board:
     """Game board"""
 
     def __init__(self) -> None:
+        self.player = 1  # 1 = Cross, 2 = Circle
         self.slots = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
+            [0, 0, 0],  # (0, 0), (0, 1), (0, 2)
+            [0, 0, 0],  # (1, 0), (1, 1), (1, 2)
+            [0, 0, 0],  # (2, 0), (2, 1), (2, 2)
         ]  # 0 = Empty, 1 = Cross, 2 = Circle
 
     def isSlotEmpty(self, row, col):
@@ -20,113 +20,64 @@ class Board:
     def updateSlot(self, row, col, player):
         self.slots[row][col] = player
 
+    def getClickedCase(self, pos):
+        row = pos[1] // 200
+        col = (pos[0] - 300) // 200
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            return (row, col)
+        return False
+
+    def setPlayer(self, player):
+        self.player = player
+
+    def getPlayer(self):
+        return self.player
+
+    def hasPlayerWon(self, pos):
+        if self.isRowComplete(pos[0]) or self.isColComplete(pos[1]):
+            return True
+
+    def isRowComplete(self, row):
+        if min(self.slots[row]) == max(self.slots[row]):
+            return True
+        return False
+
+    def isColComplete(self, col):
+        col = [self.slots[0][col], self.slots[1][col], self.slots[2][col]]
+        if min(col) == max(col):
+            return True
+        return False
+
     def placeCross(self, pos):
         self.color = (255, 0, 0)
-        if 304 < pos[0] < 496:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 0):
-                    pg.draw.line(window, self.color, (345, 45), (455, 155), 12)
-                    pg.draw.line(window, self.color, (345, 155), (455, 45), 12)
-                    self.updateSlot(0, 0, 1)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 0):
-                    pg.draw.line(window, self.color, (345, 245), (455, 355), 12)
-                    pg.draw.line(window, self.color, (345, 355), (455, 245), 12)
-                    self.updateSlot(1, 0, 1)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 0):
-                    pg.draw.line(window, self.color, (345, 445), (455, 555), 12)
-                    pg.draw.line(window, self.color, (345, 555), (455, 445), 12)
-                    self.updateSlot(2, 0, 1)
-            else:
-                pass
-        elif 504 < pos[0] < 696:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 1):
-                    pg.draw.line(window, self.color, (545, 45), (655, 155), 12)
-                    pg.draw.line(window, self.color, (545, 155), (655, 45), 12)
-                    self.updateSlot(0, 1, 1)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 1):
-                    pg.draw.line(window, self.color, (545, 245), (655, 355), 12)
-                    pg.draw.line(window, self.color, (545, 355), (655, 245), 12)
-                    self.updateSlot(1, 1, 1)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 1):
-                    pg.draw.line(window, self.color, (545, 445), (655, 555), 12)
-                    pg.draw.line(window, self.color, (545, 555), (655, 445), 12)
-                    self.updateSlot(2, 1, 1)
-            else:
-                pass
-        elif 704 < pos[0] < 896:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 2):
-                    pg.draw.line(window, self.color, (745, 45), (855, 155), 12)
-                    pg.draw.line(window, self.color, (745, 155), (855, 45), 12)
-                    self.updateSlot(0, 2, 1)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 2):
-                    pg.draw.line(window, self.color, (745, 245), (855, 355), 12)
-                    pg.draw.line(window, self.color, (745, 355), (855, 245), 12)
-                    self.updateSlot(1, 2, 1)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 2):
-                    pg.draw.line(window, self.color, (745, 445), (855, 555), 12)
-                    pg.draw.line(window, self.color, (745, 555), (855, 445), 12)
-                    self.updateSlot(2, 2, 1)
-            else:
-                pass
-        else:
-            pass
+        if self.isSlotEmpty(pos[0], pos[1]):
+            pg.draw.line(
+                window,
+                self.color,
+                (345 + 200 * pos[1], 45 + 200 * pos[0]),
+                (455 + 200 * pos[1], 155 + 200 * pos[0]),
+                12,
+            )
+            pg.draw.line(
+                window,
+                self.color,
+                (345 + 200 * pos[1], 155 + 200 * pos[0]),
+                (455 + 200 * pos[1], 45 + 200 * pos[0]),
+                12,
+            )
+            self.updateSlot(pos[0], pos[1], self.player)
+            self.setPlayer(2)
+            self.drawPlayerMark(self.getPlayer())
 
     def placeCircle(self, pos):
-        if 304 < pos[0] < 496:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 0):
-                    pg.draw.circle(window, (0, 255, 0), (400, 100), 60, 8)
-                    self.updateSlot(0, 0, 2)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 0):
-                    pg.draw.circle(window, (0, 255, 0), (400, 300), 60, 8)
-                    self.updateSlot(1, 0, 2)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 0):
-                    pg.draw.circle(window, (0, 255, 0), (400, 500), 60, 8)
-                    self.updateSlot(2, 0, 2)
-            else:
-                pass
-        elif 504 < pos[0] < 696:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 1):
-                    pg.draw.circle(window, (0, 255, 0), (600, 100), 60, 8)
-                    self.updateSlot(0, 1, 2)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 1):
-                    pg.draw.circle(window, (0, 255, 0), (600, 300), 60, 8)
-                    self.updateSlot(1, 1, 2)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 1):
-                    pg.draw.circle(window, (0, 255, 0), (600, 500), 60, 8)
-                    self.updateSlot(2, 1, 2)
-            else:
-                pass
-        elif 704 < pos[0] < 896:
-            if pos[1] < 196:
-                if self.isSlotEmpty(0, 2):
-                    pg.draw.circle(window, (0, 255, 0), (800, 100), 60, 8)
-                    self.updateSlot(0, 2, 2)
-            if 204 < pos[1] < 396:
-                if self.isSlotEmpty(1, 2):
-                    pg.draw.circle(window, (0, 255, 0), (800, 300), 60, 8)
-                    self.updateSlot(1, 2, 2)
-            if 404 < pos[1]:
-                if self.isSlotEmpty(2, 2):
-                    pg.draw.circle(window, (0, 255, 0), (800, 500), 60, 8)
-                    self.updateSlot(2, 2, 2)
-            else:
-                pass
-        else:
-            pass
+        self.color = (0, 255, 0)
+        if self.isSlotEmpty(pos[0], pos[1]):
+            pg.draw.circle(
+                window, (0, 255, 0), (400 + 200 * pos[1], 100 + 200 * pos[0]), 60, 8
+            )
+            self.updateSlot(pos[0], pos[1], self.player)
+            self.setPlayer(1)
+            self.drawPlayerMark(self.getPlayer())
 
     def drawGrid(self, color):
         pg.draw.rect(window, color, pg.Rect(300, -4, 600, 8))
@@ -154,59 +105,33 @@ class Board:
         )
         pg.display.flip()
 
-    def win_player(self,player):
-        win=False
-        for i in range(3): 
-            if self.isSlotEmpty(0, 0) = self.isSlotEmpty(1, 0) = self.isSlotEmpty(2, 0) = player:
-                win=True 
-            else: 
-                win=False
-                pass
-            elif self.isSlotEmpty(0, 1) = self.isSlotEmpty(1, 1) = self.isSlotEmpty(2, 1) = player:
-                win=True 
-            else: 
-                win=False
-                pass
-             elif self.isSlotEmpty(0, 2) = self.isSlotEmpty(1, 2) = self.isSlotEmpty(2, 2) = player:
-                win=True 
-            else: 
-                win=False
-                pass
-        pass
 
+board = Board()
+window = pg.display.set_mode((1200, 600))
+board.drawGrid((255, 255, 255))
+run = True
+board.drawPlayerMark(board.player)
 
+while run:
+    event_list = pg.event.get()
+    for event in event_list:
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = board.getClickedCase(pg.mouse.get_pos())
+                if board.getPlayer() == 1 and pos:
+                    board.placeCross(pos)
+                    if board.hasPlayerWon(pos):
+                        print("Cross Wins")
+                elif board.getPlayer() == 2 and pos:
+                    board.placeCircle(pos)
+                    if board.hasPlayerWon(pos):
+                        print("Circle Wins")
+                else:
+                    pass
 
-
-
-
-
-def main():
-    board = Board()
-    window = pg.display.set_mode((1200, 600))
-    board.drawGrid((255, 255, 255))
-    run = True
-    player = random.randint(1,2)
-    board.drawPlayerMark(1)
-
-    while run:
-        leftClick = False
-
-        event_list = pg.event.get()
-        for event in event_list:
-            if event.type == pg.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    pos = pg.mouse.get_pos()
-                    if player == 1:
-                        board.placeCross(pos)
-                        board.drawPlayerMark(2)
-                    else:
-                        board.placeCircle(pos)
-                        board.drawPlayerMark(1)
-                    player = 2 if player == 1 else 1
-                    # ! Ici on peut afficher la couleur du joueur soit avec les petits carrés soit avec toute la grille colorée
-
-        for event in event_list:
-            if event.type == pg.QUIT:
-                pg.quit()
-                running = False
-        pg.display.flip()
+    for event in event_list:
+        if event.type == pg.QUIT:
+            pg.quit()
+            running = False
+            exit(-1)
+    pg.display.flip()
